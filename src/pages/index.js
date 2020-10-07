d9d2ccb1409b0232c1f6407b42435201ba59af51
Jannik Baranczyk <jb@connect2.de>
2020-10-07 10:04:44 +0200

    Add first Lambda function (yay)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [msg, setMsg] = useState(null)
+
+  useEffect(() => {
+    async function fetchMsg() {
+      const res = await fetch("/.netlify/functions/hello-world")
+      const { message } = await res.json()
+      setMsg(message)
+    }
+
+    fetchMsg()
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{msg ? msg : "Hello my gorgeous friends on the internet!"}</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
